c8e6bf1c9f7047f14c9ea34ce22830c621659877
TylerL <tluckewicz@gmail.com>
2021-04-23 08:57:52 -0400

    add event listener to search on button click

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let myPlace = 'basking ridge, nj';
 
 
-
+// Create a card with all the information to be displayed on screen
 function createTempCard(location, temp, weather) {
     const card = document.createElement('div');
     const locationH2 = document.createElement('h2');
@@ -21,7 +21,7 @@ function createTempCard(location, temp, weather) {
 
 
 
-
+// Add the card to the DOM so it is displayed
 function appendCard(card) {
     const outputContainer = document.getElementById('output-container');
     outputContainer.appendChild(card);
@@ -30,27 +30,33 @@ function appendCard(card) {
 
 
 
-// The outer fetch function takes the text value of the location and makes a request to a geolocation API and gets an object
-// that includes the latitude and longitude of the location as a response. 
-fetch(`https://geocode.xyz/${myPlace}?region=US&geoit=json`)
-.then(function(response) {
-  return response.json();
-})
-.then(function(json) {
+function getData() {
+    const userInput = document.getElementById('location-textbox').value;
 
-    // When the latitude and longitude is returned, the inner fetch function makes a request to a weather API and gets
-    // an object that includes the current temperature and weather for the given coordinates. 
-    fetch(`https://www.7timer.info/bin/civil.php?lon=${json.longt}&lat=${json.latt}&ac=0&unit=british&output=json&tzshift=0`)
+    // The outer fetch function takes the text value of the location and makes a request to a geolocation API and gets an object
+    // that includes the latitude and longitude of the location as a response. 
+    fetch(`https://geocode.xyz/${userInput}?region=US&geoit=json`)
     .then(function(response) {
-        return response.json();
+    return response.json();
     })
     .then(function(json) {
-        let data = createTempCard(myPlace, json.dataseries[0].temp2m, json.dataseries[0].weather);
-        appendCard(data);
+
+        // When the latitude and longitude is returned, the inner fetch function makes a request to a weather API and gets
+        // an object that includes the current temperature and weather for the given coordinates. 
+        fetch(`https://www.7timer.info/bin/civil.php?lon=${json.longt}&lat=${json.latt}&ac=0&unit=british&output=json&tzshift=0`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            let data = createTempCard(userInput, json.dataseries[0].temp2m, json.dataseries[0].weather);
+            appendCard(data);
+            console.log(json);
+        });
+
     });
 
-});
+}
 
 
-
-
+const button = document.getElementById('weather-btn');
+button.addEventListener('click', getData);
